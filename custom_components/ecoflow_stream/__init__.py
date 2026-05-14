cb3e3c7c3295c0dc0363a198ac693a5c3a3dd9c8
Denisk1990 <105648069+Denisk1990@users.noreply.github.com>
2026-05-14 11:24:50 +0200

    Update __init__.py

--- custom_components/ecoflow_stream/__init__.py
+++ custom_components/ecoflow_stream/__init__.py
@@ -2,33 +2,75 @@
 
 from __future__ import annotations
 
+from datetime import timedelta
+import logging
+
+import aiohttp
+
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 
+from .api import EcoFlowStreamApi
 from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
 
 PLATFORMS = ["sensor"]
 
 
 async def async_setup(hass: HomeAssistant, config: dict) -> bool:
-    """Set up the EcoFlow STREAM component."""
+    """Set up EcoFlow STREAM."""
     hass.data.setdefault(DOMAIN, {})
     return True
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up EcoFlow STREAM from a config entry."""
-    hass.data.setdefault(DOMAIN, {})
 
-    hass.data[DOMAIN][entry.entry_id] = {}
+    access_key = entry.data["access_key"]
+    secret_key = entry.data["secret_key"]
+    device_sn = entry.data["device_sn"]
 
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    session = aiohttp.ClientSession()
+
+    api = EcoFlowStreamApi(
+        access_key,
+        secret_key,
+        device_sn,
+    )
+
+    async def async_update_data():
+        """Fetch data from API."""
+        try:
+            return await api.async_get_all_quota(session)
+        except Exception as err:
+            _LOGGER.error("Error updating EcoFlow data: %s", err)
+            return {}
+
+    coordinator = DataUpdateCoordinator(
+        hass,
+        _LOGGER,
+        name="ecoflow_stream",
+        update_method=async_update_data,
+        update_interval=timedelta(seconds=10),
+    )
+
+    await coordinator.async_config_entry_first_refresh()
+
+    hass.data[DOMAIN][entry.entry_id] = coordinator
+
+    await hass.config_entries.async_forward_entry_setups(
+        entry,
+        PLATFORMS,
+    )
 
     return True
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Unload a config entry."""
+    """Unload entry."""
+
     unload_ok = await hass.config_entries.async_unload_platforms(
         entry,
         PLATFORMS,
